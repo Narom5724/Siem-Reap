f2c22a00571c98c0257b5d1399d123a64a39ae3d
Narom5724 <roennarom@gmail.com>
2025-08-27 20:45:19 +0700

    siem reap

---
+++ js/app.js
@@ -0,0 +1,5 @@
+const navbar = document.querySelector(".responsive-nav");
+const hambuger = document.querySelector(".hambuger");
+hambuger.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
---
+++ js/app2.js
@@ -0,0 +1,7 @@
+const navbar = document.querySelector(".responsive-nav");
+const hambuger = document.querySelector(".hambuger");
+hambuger.addEventListener("click", () => {
+  navbar.classList.toggle("active");
+});
+
+
